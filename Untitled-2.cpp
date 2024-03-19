ddf2317779b0a3dbcf85b8e0b5a3f1bf683764b2
Edgvvr <164045278+Edgvvr@users.noreply.github.com>
2024-03-19 10:47:02 -0500

    Segundo codigo
    segunda descripcion de codigo

---
+++ Untitled-2.cpp
@@ -0,0 +1,76 @@
+// C++ code
+//
+int led1=13;
+int led2=12;
+int led3=11;
+int led4=10;
+int led5=9;
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led1, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+    digitalWrite(led2, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led2, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+
+   digitalWrite(led3, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led3, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+   digitalWrite(led4, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led4, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(led5, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led5, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+
+  digitalWrite(led5, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led5, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(led4, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led4, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(led3, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led3, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+
+   digitalWrite(led2, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led2, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+   digitalWrite(led1, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(led1, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+ 
+  
+
+ 
+  
+}
